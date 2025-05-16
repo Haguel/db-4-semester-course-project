6c9c19968b19c589ff1bb90a066c378c4c91db6c
GlebHal <hammanov.gleb@gmail.com>
2025-05-16 12:33:15 +0300

    update roles.sql

--- ROLES.sql
+++ ROLES.sql
@@ -24,6 +24,4 @@ GRANT auditor TO auditor_user;
 GRANT SELECT ON books, authors, categories, publishers, branches,
     book_authors, borrowers, loans, reservations, staff TO auditor;
 
--- Важно: если используются SERIAL поля,
--- дополнительно даём права на последовательности (чтобы INSERT работал без ошибок)
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO librarian, auditor, reader;
